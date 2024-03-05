560dfba909d111a3c9ba0b3f693b52c3a6fc3b2c
SIPHAI <phalsiphai18@gmail.com>
2024-03-05 16:12:53 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
-    } else {
+      } else {
       x.className = "topnav";
     }
-    }
+  }
