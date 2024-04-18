88636c941a29f9c180da5dc3c6aabdfcf07b367c
SIPHAI <phalsiphai18@gmail.com>
2024-04-18 11:28:12 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-      } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
   }
+}
+var header = document.getElementById('myTopnav');
+var link = header.getElementsByClassName('a');
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
+  });
+}
+
+
+
